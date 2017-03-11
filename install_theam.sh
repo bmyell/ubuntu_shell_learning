dd46f25944e6d4b04b70dad7d56c95ede8e745d4
bmyell <1812630796@qq.com>
2017-03-11 16:56:31 +0800

    add install theme

---
+++ install_theam.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#install ubuntu-tweak
+sudo add-apt-repository ppa:tualatrix/ppa -y  
+sudo apt-get update
+sudo apt-get install ubuntu-tweak
+
+sudo add-apt-repository ppa:noobslab/icons
+sudo apt-get update
+sudo apt-get install ultra-flat-icons
+
