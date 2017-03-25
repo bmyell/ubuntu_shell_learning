03a933524c1baa30dd6a2a101ab84dbbc2bb3556
bmyell <1812630796@qq.com>
2017-03-25 16:44:23 +0800

    h

--- install_theme.sh
+++ install_theme.sh
@@ -2,9 +2,9 @@
 #install ubuntu-tweak
 sudo add-apt-repository ppa:tualatrix/ppa -y  
 sudo apt-get update
-sudo apt-get install ubuntu-tweak
+sudo apt-get install ubuntu-tweak -y
 
-sudo add-apt-repository ppa:noobslab/icons
+sudo add-apt-repository ppa:noobslab/icons -y
 sudo apt-get update
-sudo apt-get install ultra-flat-icons
+sudo apt-get install ultra-flat-icons -y
 
