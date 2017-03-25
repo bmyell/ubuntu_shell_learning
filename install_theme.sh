a2725ace234a37ac971b07fcd07c30a1408851c6
bmyell <1812630796@qq.com>
2017-03-25 16:55:20 +0800

    hah

--- install_theme.sh
+++ install_theme.sh
@@ -2,7 +2,7 @@
 #install ubuntu-tweak
 sudo add-apt-repository ppa:tualatrix/ppa -y  
 sudo apt-get update
-sudo apt-get install ubuntu-tweak -y
+sudo apt-get install unity-tweak-tool  -y
 
 sudo add-apt-repository ppa:noobslab/icons -y
 sudo apt-get update
