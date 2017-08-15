14f101a1fdb6acd12495701702bb479c21255c96
bmyell <1812630796@qq.com>
2017-08-15 22:58:19 +0900

    add git_init.sh some configure

--- git_init.sh
+++ git_init.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
+#Before you can start using Git, you need to configure it. Run each of the following lines on the command line to make sure everything is set up.
+
+#set up Git with your name 
 git config --global user.email "1812630796@qq.com"
+
+#set up Git with your email
 git config --global user.name "bmyell"
+
+#make sure that Git outpout is colored
+git config --global color.ui auto
+
+#displays the original state in conflict 
+git config --global merge.conflictstyle diff3
+
+git config --list
+
+#add rsa pub 
 ssh-keygen -t rsa -b 4096 -C "1812630796@qq.com"
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/id_rsa
-
 
 
 
---
+++ git_init.sh~
@@ -0,0 +1,11 @@
+#!/bin/bash
+git config --global user.email "1812630796@qq.com"
+git config --global user.name "bmyell"
+ssh-keygen -t rsa -b 4096 -C "1812630796@qq.com"
+eval $(ssh-agent -s)
+ssh-add ~/.ssh/id_rsa
+
+
+
+
+
