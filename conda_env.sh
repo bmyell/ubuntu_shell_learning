18058251ae85da83e805a67a388fcb13b50da31a
bmyell <1812630796@qq.com>
2018-06-10 19:25:44 -0700

    hello

---
+++ conda_env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/
+conda config --set show_channel_urls yes
+
